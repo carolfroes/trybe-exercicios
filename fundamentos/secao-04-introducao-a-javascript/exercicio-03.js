64e50e234c7fc1bbcd1692135c9d891b4d57dca5
Carolina <carolfroes.1@hotmail.com>
2022-11-25 14:34:17 -0400

    atualiza atividades de javascript, adiciona atividade 2

--- fundamentos/secao-04-introducao-a-javascript/exercicio-03.js
+++ fundamentos/secao-04-introducao-a-javascript/exercicio-03.js
@@ -1,8 +1,9 @@
-const grade = 90
-if (grade >= 90) {
-    console.log('Parabéns, você foi aprovada(o)!')
-} else if (grade < 90 && grade >= 60) {
-    console.log('Você está na nossa lista de espera')
-} else if (grade < 60) {
-    console.log('Você foi reprovada(o)')
-}
+const base = 5;
+const height = 8;
+const area = base * height;
+const perimeter = 5 + 5 + 8 + 8;
+
+console.log(area);
+console.log(perimeter);
+
+
