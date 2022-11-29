d50c48159e1e857aba09bc90314206b961fffd18
Carolina <carolfroes.1@hotmail.com>
2022-11-28 23:08:07 -0400

    finaliza exercício 3, seção 4, dia 3

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01/exercicio-03.js
@@ -0,0 +1,8 @@
+const grade = 40;
+if (grade >= 80) {
+    console.log('Parabéns, você foi aprovado(a)!');
+} else if (grade < 80 && grade >= 60) {
+    console.log('Você está na nossa lista de espera');
+} else if (grade < 60) {
+    console.log('Você foi reprovado(a)');
+}
