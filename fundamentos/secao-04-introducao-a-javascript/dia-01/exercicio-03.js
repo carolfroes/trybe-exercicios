4ad1cdcc2582a1f0871054caf4e78b725dd860ce
Carolina <carolfroes.1@hotmail.com>
2022-11-30 09:52:36 -0400

    refazendo exercício 3, seção 4

--- fundamentos/secao-04-introducao-a-javascript/dia-01/exercicio-03.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01/exercicio-03.js
@@ -1,8 +1,12 @@
 const grade = 80;
+
 if (grade >= 80) {
-    console.log('Parabéns, você foi aprovado(a)!');
+    console.log('Parabéns, você foi aprovado(a)');
 } else if (grade < 80 && grade >= 60) {
-    console.log('Você está na nossa lista de espera');
-} else if (grade < 60) {
-    console.log('Você foi reprovado(a)');
+    console.log('Você está na nossa lista de espera')
+
+} else if (grade <= 60) {
+    console.log('Você foi reprovado(a)')
+} else {
+    console.log('Nota inválida');
 }
