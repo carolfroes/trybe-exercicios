d2d535ecb924c93634a681aaebed4729c4c4716d
Carolina <carolfroes.1@hotmail.com>
2022-11-30 17:30:56 -0400

    finaliza exercicio 11, seção 4, dia 1

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01/exercicio-11.js
@@ -0,0 +1,21 @@
+// 🚀
+const a = 60;
+const b = 60;
+const c = 60;
+
+//let sumOfAngles = a > b && b > 0 && c > 00;
+//let allAnglesArePositives = a + b + c;
+
+if (a > 0 && b > 0 && c > 0) {
+    if (a + b + c === 180) {
+        console.log('True');
+    } else {
+        console.log('False');
+    };
+
+} else {
+    console.log('Erro: ângulo inválido')
+}
+
+
+
