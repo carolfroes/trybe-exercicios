7cb15d8b93f72d6367d5d3b01fc7fb19f2201f24
Carolina <carolfroes.1@hotmail.com>
2022-11-30 22:23:26 -0400

    finaliza exercício 15, seção 4, dia 1

--- fundamentos/secao-04-introducao-a-javascript/dia-01/exercicio-14.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01/exercicio-14.js
@@ -4,7 +4,7 @@ const numberC = 2;
 
 let isEven = false;
 
-if ((numberA / 1 === 0 || numberB / 1 === 0 || numberC / 1 === 0)) {
+if ((numberA % 1 === 0 || numberB % 1 === 0 || numberC % 1 === 0)) {
 
 };
 console.log(isEven);
