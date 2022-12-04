0d9f82396210817b35c80b345ec0ada1707523d6
Carolina <carolfroes.1@hotmail.com>
2022-12-04 16:14:11 -0400

    finaliza exercício 3.5

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01/exercicio-3.5.js
@@ -0,0 +1,12 @@
+const comida = 'pão na chapa';
+const bebida = 'cafézinho';
+
+if (bebida === 'cafézinho' && comida === 'pão na chapa') {
+    console.log('Muito obrigado pela refeição :)');
+} else {
+    console.log('Acho que houve um engano com meu pedido');
+};
+
+const conditionOne = true;
+const conditionTwo = false;
+console.log(conditionOne && conditionTwo);
