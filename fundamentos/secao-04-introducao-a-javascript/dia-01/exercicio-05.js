cc8c015babbe2673569f6201eb6ff6f9bc9b7588
Carolina <carolfroes.1@hotmail.com>
2022-12-04 20:40:34 -0400

    adiciona exercício 5.5

--- fundamentos/secao-04-introducao-a-javascript/dia-01/exercicio-05.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01/exercicio-05.js
@@ -1,7 +1,7 @@
-const weekDay = 'sábado';
+const weekDay = 'Sábado';
 
 if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
-    console.log('Oba, mais um dia de aprendizado na Trybe >:D')
-} else[
-    console.log('FINALMENTE, descanso merecido UwU')
-]
+    console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+} else {
+    console.log('FINALMENTE, descanso merecido UwU');
+}
