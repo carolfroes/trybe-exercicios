5bcf583f8408049265fea0480874dcf1be1f775e
Carolina <carolfroes.1@hotmail.com>
2022-12-08 02:27:10 -0400

    finaliza exercício 2.6

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02/exercicio-2.6.js
@@ -0,0 +1,6 @@
+let listaDeNomes = ['Joana', 'Maria', 'Lucas'];
+
+for (let indice = 0; indice < listaDeNomes.length; indice++) {
+    let mensagem = 'Boas vindas,' + listaDeNomes[indice] + '!';
+    console.log(mensagem);
+}
